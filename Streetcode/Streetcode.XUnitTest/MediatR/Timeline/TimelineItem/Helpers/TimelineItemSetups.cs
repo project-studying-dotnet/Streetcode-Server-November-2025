7670c832993fb6d8de71d48719b8e1f2ab2cf0cc
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-11-23 17:16:13 +0200

    test/2(TimelineItem): add TimelineItemSetups class for repository wrapper setup in unit tests

---
+++ Streetcode/Streetcode.XUnitTest/MediatR/Timeline/TimelineItem/Helpers/TimelineItemSetups.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.DAL.Repositories.Interfaces.Timeline;
+
+namespace Streetcode.XUnitTest.MediatR.Timeline.TimelineItem.Helpers
+{
+    public static class TimelineItemSetups
+    {
+        public static void SetupRepositoryWrapper(this Mock<IRepositoryWrapper> repositoryWrapperMock, Mock<ITimelineRepository> timelineRepositoryMock)
+        {
+            repositoryWrapperMock
+                .Setup(rw => rw.TimelineRepository)
+                .Returns(timelineRepositoryMock.Object);
+        }
+    }
+}
