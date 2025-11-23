0c0bef6ba370211c72bad0e1a7c5b0599e1ae3a8
VolodymyrS <volodymyr.sh.05@gmail.com>
2025-11-23 20:07:07 +0200

    test/2(TimelineItem): add SetupLogger method for configuring logger in unit tests

--- Streetcode/Streetcode.XUnitTest/MediatR/Timeline/TimelineItem/Helpers/TimelineItemSetups.cs
+++ Streetcode/Streetcode.XUnitTest/MediatR/Timeline/TimelineItem/Helpers/TimelineItemSetups.cs
@@ -3,10 +3,11 @@
     using System.Linq.Expressions;
     using Microsoft.EntityFrameworkCore.Query;
     using Moq;
+    using Streetcode.BLL.Interfaces.Logging;
     using Streetcode.DAL.Entities.Timeline;
     using Streetcode.DAL.Repositories.Interfaces.Base;
     using Streetcode.DAL.Repositories.Interfaces.Timeline;
-    
+
     public static class TimelineItemSetups
     {
         public static void SetupRepositoryWrapper(this Mock<IRepositoryWrapper> repositoryWrapperMock, Mock<ITimelineRepository> timelineRepositoryMock)
@@ -23,6 +24,14 @@
                     It.IsAny<Expression<Func<TimelineItem, bool>>>(),
                     It.IsAny<Func<IQueryable<TimelineItem>, IIncludableQueryable<DAL.Entities.Timeline.TimelineItem, object>>>()))
                 .ReturnsAsync(timelineItems!);
+        }
+
+        public static void SetupLogger(this Mock<ILoggerService> loggerMock)
+        {
+            loggerMock
+                .Setup(l => l.LogError(
+                    It.IsAny<object>(),
+                    It.IsAny<string>()));
         }
     }
 }
