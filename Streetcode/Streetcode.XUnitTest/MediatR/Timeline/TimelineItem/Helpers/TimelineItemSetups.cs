931adaa806bbfc2a8a303cfe7c8951ea3a815e8f
VolodymyrS <volodymyr.sh.05@gmail.com>
2025-11-23 19:55:43 +0200

    test/2(TimelineItem): add SetupTimelineRepository method for configuring timeline repository in unit tests

--- Streetcode/Streetcode.XUnitTest/MediatR/Timeline/TimelineItem/Helpers/TimelineItemSetups.cs
+++ Streetcode/Streetcode.XUnitTest/MediatR/Timeline/TimelineItem/Helpers/TimelineItemSetups.cs
@@ -1,9 +1,12 @@
-﻿using Moq;
-using Streetcode.DAL.Repositories.Interfaces.Base;
-using Streetcode.DAL.Repositories.Interfaces.Timeline;
-
-namespace Streetcode.XUnitTest.MediatR.Timeline.TimelineItem.Helpers
+﻿namespace Streetcode.XUnitTest.MediatR.Timeline.TimelineItem.Helpers
 {
+    using System.Linq.Expressions;
+    using Microsoft.EntityFrameworkCore.Query;
+    using Moq;
+    using Streetcode.DAL.Entities.Timeline;
+    using Streetcode.DAL.Repositories.Interfaces.Base;
+    using Streetcode.DAL.Repositories.Interfaces.Timeline;
+    
     public static class TimelineItemSetups
     {
         public static void SetupRepositoryWrapper(this Mock<IRepositoryWrapper> repositoryWrapperMock, Mock<ITimelineRepository> timelineRepositoryMock)
@@ -11,6 +14,15 @@ namespace Streetcode.XUnitTest.MediatR.Timeline.TimelineItem.Helpers
             repositoryWrapperMock
                 .Setup(rw => rw.TimelineRepository)
                 .Returns(timelineRepositoryMock.Object);
+        }
+
+        public static void SetupTimelineRepository(this Mock<ITimelineRepository> timelineRepositoryMock, List<TimelineItem>? timelineItems)
+        {
+            timelineRepositoryMock
+                .Setup(r => r.GetAllAsync(
+                    It.IsAny<Expression<Func<TimelineItem, bool>>>(),
+                    It.IsAny<Func<IQueryable<TimelineItem>, IIncludableQueryable<DAL.Entities.Timeline.TimelineItem, object>>>()))
+                .ReturnsAsync(timelineItems!);
         }
     }
 }
