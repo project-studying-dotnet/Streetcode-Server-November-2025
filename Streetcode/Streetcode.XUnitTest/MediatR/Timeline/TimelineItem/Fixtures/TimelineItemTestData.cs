604393786268642d88c87e870bf9aba7d16ef3ea
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-11-24 14:57:29 +0200

    docs/2(TimelineItem): add XML documentation for TimelineItemTestData factory methods

--- Streetcode/Streetcode.XUnitTest/MediatR/Timeline/TimelineItem/Fixtures/TimelineItemTestData.cs
+++ Streetcode/Streetcode.XUnitTest/MediatR/Timeline/TimelineItem/Fixtures/TimelineItemTestData.cs
@@ -4,8 +4,18 @@
     using Streetcode.DAL.Entities.Timeline;
     using Streetcode.DAL.Enums;
 
+    /// <summary>
+    /// Provides factory methods for creating test instances of <see cref="TimelineItem"/>
+    /// and <see cref="TimelineItemDTO"/> objects for use in unit tests.
+    /// </summary>
     public static class TimelineItemTestData
     {
+        /// <summary>
+        /// Creates a single <see cref="TimelineItem"/> entity instance with predefined values.
+        /// </summary>
+        /// <param name="id">The ID of the timeline item.</param>
+        /// <param name="streetcodeId">The streetcode ID associated with the item.</param>
+        /// <returns>A fully initialized <see cref="TimelineItem"/> object for testing.</returns>
         public static TimelineItem CreateTimelineItem(int id = 1, int streetcodeId = 101)
         {
             return new TimelineItem
@@ -21,6 +31,13 @@
             };
         }
 
+        /// <summary>
+        /// Creates a collection of <see cref="TimelineItem"/> entities with sequential IDs
+        /// and automatically increasing years.
+        /// </summary>
+        /// <param name="count">The number of timeline items to generate.</param>
+        /// <param name="streetcodeId">The streetcode ID for all items.</param>
+        /// <returns>A list of <see cref="TimelineItem"/> objects.</returns>
         public static List<TimelineItem> CreateTimelineItems(int count = 5, int streetcodeId = 101)
         {
             var items = new List<TimelineItem>(count);
@@ -43,6 +60,11 @@
             return items;
         }
 
+        /// <summary>
+        /// Creates a single <see cref="TimelineItemDTO"/> instance with predefined values.
+        /// </summary>
+        /// <param name="id">The ID of the timeline item DTO.</param>
+        /// <returns>A fully initialized <see cref="TimelineItemDTO"/> object for testing.</returns>
         public static TimelineItemDTO CreateTimelineItemDTO(int id = 1)
         {
             return new TimelineItemDTO
@@ -56,6 +78,12 @@
             };
         }
 
+        /// <summary>
+        /// Creates a collection of <see cref="TimelineItemDTO"/> objects with sequential IDs
+        /// and automatically increasing years.
+        /// </summary>
+        /// <param name="count">The number of DTO items to create.</param>
+        /// <returns>A list of <see cref="TimelineItemDTO"/> instances.</returns>
         public static List<TimelineItemDTO> CreateTimelineItemDTOs(int count = 5)
         {
             var items = new List<TimelineItemDTO>(count);
