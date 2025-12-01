4b73578d28e725ca2ee6de08b811855205c3307c
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-12-01 14:39:53 +0200

    feat/74: implement update functionality for interesting facts with UpdateFact command and handler

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Update/UpdateFactCommand.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Update
+{
+    public record UpdateFactCommand(UpdateFactDto updateFact) : IRequest<Result<FactDto>>;
+}
---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Update/UpdateFactHandler.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Update;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Fact.Update
+{
+    public class UpdateFactHandler : IRequestHandler<UpdateFactCommand, Result<FactDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
+
+        public UpdateFactHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Result<FactDto>> Handle(UpdateFactCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var existingFact = await _repositoryWrapper.FactRepository.GetFirstOrDefaultAsync(f => f.Id == request.updateFact.Id);
+
+                if (existingFact is null)
+                {
+                    return Result.Fail("Fact not found");
+                }
+
+                _mapper.Map(request.updateFact, existingFact);
+
+                _repositoryWrapper.FactRepository.Update(existingFact);
+                await _repositoryWrapper.SaveChangesAsync();
+
+                return Result.Ok(_mapper.Map<FactDto>(existingFact));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(request, ex.Message);
+                return Result.Fail(ex.Message);
+            }
+        }
+    }
+}
