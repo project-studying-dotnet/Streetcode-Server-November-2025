8c17ec43df1615affc9e69b613e43f405dc8d16d
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-12-01 14:40:12 +0200

    feat/74: implement delete functionality for interesting facts with DeleteFact command and handler

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Delete/DeleteFactHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Create;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Delete
+{
+    public class DeleteFactHandler : IRequestHandler<DeleteFactCommand, Result<Unit>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
+
+        public DeleteFactHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Result<Unit>> Handle(DeleteFactCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var deletedFact = await _repositoryWrapper.FactRepository.GetFirstOrDefaultAsync(f => f.Id == request.id);
+
+                if (deletedFact is null)
+                {
+                    return Result.Fail("Fact not found");
+                }
+
+                _repositoryWrapper.FactRepository.Delete(deletedFact);
+
+                await _repositoryWrapper.SaveChangesAsync();
+                return Result.Ok(Unit.Value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(request, ex.Message);
+                return Result.Fail(ex.Message);
+            }
+        }
+    }
+}
