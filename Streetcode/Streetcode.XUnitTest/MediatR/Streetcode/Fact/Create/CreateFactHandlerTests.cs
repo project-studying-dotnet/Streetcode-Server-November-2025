bce2958d2ed57cee5f90434182ac5173bebb7ead
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-12-02 21:30:11 +0200

    test/74: add unit tests templates for CreateFactHandler

---
+++ Streetcode/Streetcode.XUnitTest/MediatR/Streetcode/Fact/Create/CreateFactHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿namespace Streetcode.XUnitTest.MediatR.Fact.Create
+{
+    using AutoMapper;
+    using Moq;
+    using Streetcode.BLL.Interfaces.Logging;
+    using Streetcode.BLL.MediatR.Fact.Create;
+    using Streetcode.DAL.Repositories.Interfaces.Base;
+    using Xunit;
+
+    public class CreateFactHandlerTests
+    {
+        private readonly Mock<IMapper> mapperMock;
+        private readonly Mock<IRepositoryWrapper> repositoryWrapperMock;
+        private readonly Mock<ILoggerService> loggerMock;
+        private readonly CreateFactHandler handler;
+
+        public CreateFactHandlerTests()
+        {
+            this.mapperMock = new Mock<IMapper>();
+            this.repositoryWrapperMock = new Mock<IRepositoryWrapper>();
+            this.loggerMock = new Mock<ILoggerService>();
+            this.handler = new CreateFactHandler(
+                this.repositoryWrapperMock.Object,
+                this.mapperMock.Object,
+                this.loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_WhenImageExists_ShouldReturnFactDto()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public async Task Handle_WhenImageDoesNotExist_ShouldReturnFailureResult()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public async Task Handle_WhenStreetcodeDoesNotExist_ShouldReturnFailureResult()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public async Task Handle_WhenFactDoesNotExist_ShouldReturnFailureResult()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public async Task Handle_WhenCreateFactDtoMappingFails_ShouldReturnFailureResult()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
