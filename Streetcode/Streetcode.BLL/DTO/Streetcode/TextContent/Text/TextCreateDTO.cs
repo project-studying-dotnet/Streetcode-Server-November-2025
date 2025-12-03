48764cdc56b94bdd734514f270925c4c87cecfc0
LekhivOleh <oleh.lekhiv.ir.2023@lpnu.ua>
2025-12-03 21:33:51 +0200

    feat/56: update dto's to include new fields

--- Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Text/TextCreateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Text/TextCreateDTO.cs
@@ -5,5 +5,8 @@ namespace Streetcode.BLL.DTO.Streetcode.TextContent.Text
     public string Title { get; set; }
     public string TextContent { get; set; }
     public string? AdditionalText { get; set; }
+    public string? VideoUrl { get; set; }
+    public string? Authorship { get; set; }
+    public int StreetcodeId { get; set; }
   }
 }
