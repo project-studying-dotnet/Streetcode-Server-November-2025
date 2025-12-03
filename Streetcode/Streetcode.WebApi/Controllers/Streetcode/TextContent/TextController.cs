c6bd638c2a70bc691c22fbc20bc46658e7a7b95b
LekhivOleh <oleh.lekhiv.ir.2023@lpnu.ua>
2025-12-03 21:41:49 +0200

    feat/56: add new create, update and delete endpoints to controller

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/TextController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/TextController.cs
@@ -1,10 +1,13 @@
 using FluentResults;
 using Microsoft.AspNetCore.Mvc;
-using Streetcode.BLL.DTO.Streetcode.TextContent;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Text;
+using Streetcode.BLL.MediatR.Streetcode.Text.Create;
+using Streetcode.BLL.MediatR.Streetcode.Text.Delete;
 using Streetcode.BLL.MediatR.Streetcode.Text.GetAll;
 using Streetcode.BLL.MediatR.Streetcode.Text.GetById;
 using Streetcode.BLL.MediatR.Streetcode.Text.GetByStreetcodeId;
 using Streetcode.BLL.MediatR.Streetcode.Text.GetParsed;
+using Streetcode.BLL.MediatR.Streetcode.Text.Update;
 
 namespace Streetcode.WebApi.Controllers.Streetcode.TextContent;
 
@@ -32,5 +35,23 @@ public class TextController : BaseApiController
     public async Task<IActionResult> GetParsedText([FromQuery] string text)
     {
         return HandleResult(await Mediator.Send(new GetParsedTextForAdminPreviewCommand(text)));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] TextCreateDTO textDTO)
+    {
+        return HandleResult(await Mediator.Send(new CreateTextCommand(textDTO)));
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] TextCreateDTO textDTO)
+    {
+        return HandleResult(await Mediator.Send(new UpdateTextCommand(id, textDTO)));
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        return HandleResult(await Mediator.Send(new DeleteTextCommand(id)));
     }
 }
