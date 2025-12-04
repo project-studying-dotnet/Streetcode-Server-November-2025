327d441bd687ac43b6400b14498c946292d7d297
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-12-04 15:24:59 +0200

    feat/74: update field restrictions for Title in image_details and facts tables
    - Increased maxLength of Title in image_details from 100 to 200.
    - Decreased maxLength of Title in facts from 100 to 68.

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20251204132401_ChangeFieldRestrictions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFieldRestrictions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "media",
+                table: "image_details",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "streetcode",
+                table: "facts",
+                type: "nvarchar(68)",
+                maxLength: 68,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "media",
+                table: "image_details",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "streetcode",
+                table: "facts",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(68)",
+                oldMaxLength: 68);
+        }
+    }
+}
