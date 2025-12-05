f57084b599e6c0c8195cfccc1896a26ae8315790
Antonina Ivanova <a.ivanova.lviv@gmail.com>
2025-12-05 15:07:42 +0200

    feat: Add UpdatePartner validator
    Issue #119

---
+++ Streetcode/Streetcode.BLL/MediatR/Partners/Update/UpdatePartnerQueryValidator.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+
+namespace Streetcode.BLL.MediatR.Partners.Update
+{
+    /// <summary>
+    /// Validator for UpdatePartnerQuery.
+    /// </summary>
+    public class UpdatePartnerQueryValidator : AbstractValidator<UpdatePartnerQuery>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdatePartnerQueryValidator"/> class.
+        /// </summary>
+        public UpdatePartnerQueryValidator()
+        {
+            RuleFor(x => x.Partner)
+                .NotNull()
+                .WithMessage("Partner data is required")
+                .SetValidator(new Create.CreatePartnerDtoValidator());
+
+            RuleFor(x => x.Partner.Id)
+                .GreaterThan(0)
+                .When(x => x.Partner != null)
+                .WithMessage("Partner Id must be greater than 0");
+        }
+    }
+}
