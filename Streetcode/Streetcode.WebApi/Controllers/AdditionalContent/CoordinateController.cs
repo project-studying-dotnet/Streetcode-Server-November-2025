c9c6616a2b132988e526cb8fdd5f3fa685c0470f
Shribak Bohdan <shribakb@gmail.com>
2025-12-10 14:50:51 +0200

    feat/132: add create update delete methods in coordinate controller

--- Streetcode/Streetcode.WebApi/Controllers/AdditionalContent/CoordinateController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/AdditionalContent/CoordinateController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.DTO.AdditionalContent.Coordinates.Types;
 using Streetcode.BLL.DTO.AdditionalContent.Subtitles;
+using Streetcode.BLL.MediatR.AdditionalContent.Coordinate.Create;
+using Streetcode.BLL.MediatR.AdditionalContent.Coordinate.Delete;
 using Streetcode.BLL.MediatR.AdditionalContent.Coordinate.GetByStreetcodeId;
+using Streetcode.BLL.MediatR.AdditionalContent.Coordinate.Update;
 
 namespace Streetcode.WebApi.Controllers.AdditionalContent;
 
@@ -10,5 +14,28 @@ public class CoordinateController : BaseApiController
     public async Task<IActionResult> GetByStreetcodeId([FromRoute] int streetcodeId)
     {
         return HandleResult(await Mediator.Send(new GetCoordinatesByStreetcodeIdQuery(streetcodeId)));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        [FromBody] StreetcodeCoordinateDto streetcodeCoordinate)
+    {
+        return HandleResult(await Mediator.Send(new CreateCoordinateCommand(streetcodeCoordinate)));
+    }
+
+    [HttpPut("{coordinateId:int}")]
+    public async Task<IActionResult> Update(
+        [FromRoute] int coordinateId,
+        [FromBody] StreetcodeCoordinateDto dto)
+    {
+        dto.Id = coordinateId;
+
+        return HandleResult(await Mediator.Send(new UpdateCoordinateCommand(dto)));
+    }
+
+    [HttpDelete("{coordinateId:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int coordinateId)
+    {
+        return HandleResult(await Mediator.Send(new DeleteCoordinateCommand(coordinateId)));
     }
 }
