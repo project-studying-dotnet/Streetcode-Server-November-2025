093d8168d6ef060995f4bdb282271a5fcb71d7a9
Artem Terzi <111956601+ArtemTerzi@users.noreply.github.com>
2025-12-12 00:37:47 +0200

    Added CreateCoordinateCommandValidatorTests and StreetcodeCoordinateDtoValidatorTests

---
+++ Streetcode/Streetcode.XUnitTest/MediatR/AdditionalContent/Coordinate/Create/StreetcodeCoordinateDtoValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿namespace Streetcode.XUnitTest.MediatR.AdditionalContent.Coordinate.Create
+{
+    using FluentValidation.TestHelper;
+    using Streetcode.BLL.DTO.AdditionalContent.Coordinates.Types;
+    using Streetcode.BLL.MediatR.AdditionalContent.Coordinate.Create;
+    using Streetcode.BLL.Util.Validators;
+    using Xunit;
+
+    public class StreetcodeCoordinateDtoValidatorTests
+    {
+        private readonly StreetcodeCoordinateDtoValidator _validator;
+
+        public StreetcodeCoordinateDtoValidatorTests()
+        {
+            _validator = new StreetcodeCoordinateDtoValidator();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_StreetcodeId_Is_Invalid()
+        {
+            // Arrange
+            var dto = new StreetcodeCoordinateDto { StreetcodeId = ValidationConstants.Common.MinPositiveValue };
+
+            // Act
+            var result = _validator.TestValidate(dto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.StreetcodeId);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Latitude_Is_Too_Low()
+        {
+            // Arrange
+            var dto = new StreetcodeCoordinateDto { Latitude = ValidationConstants.Coordinate.MinLatitude - 0.0001m };
+
+            // Act
+            var result = _validator.TestValidate(dto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Latitude);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Latitude_Is_Too_High()
+        {
+            // Arrange
+            var dto = new StreetcodeCoordinateDto { Latitude = ValidationConstants.Coordinate.MaxLatitude + 0.0001m };
+
+            // Act
+            var result = _validator.TestValidate(dto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Latitude);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Dto_Is_Valid()
+        {
+            // Arrange
+            var dto = new StreetcodeCoordinateDto
+            {
+                StreetcodeId = 1,
+                Latitude = 0,
+                Longtitude = 0,
+                Id = 1
+            };
+
+            // Act
+            var result = _validator.TestValidate(dto);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
