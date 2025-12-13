18763f482ba121a00e78a7793376ebcc5347178a
Artem Terzi <111956601+ArtemTerzi@users.noreply.github.com>
2025-12-13 18:26:50 +0200

    Added NewsValidators Tests

---
+++ Streetcode/Streetcode.XUnitTest/MediatR/Newss/Create/CreateNewsCommandValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Streetcode.XUnitTest.MediatR.Newss.Create
+{
+    using FluentValidation.TestHelper;
+    using Streetcode.BLL.DTO.News;
+    using Streetcode.BLL.MediatR.Newss.Create;
+    using Xunit;
+
+    public class CreateNewsCommandValidatorTests
+    {
+        private readonly CreateNewsCommandValidator _validator;
+
+        public CreateNewsCommandValidatorTests()
+        {
+            _validator = new CreateNewsCommandValidator();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_News_Is_Null()
+        {
+            var command = new CreateNewsCommand(null);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(x => x.newNews);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Child_Validator_Fails()
+        {
+            var invalidDto = new NewsDto { Title = string.Empty };
+            var command = new CreateNewsCommand(invalidDto);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.newNews.Title);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_News_Is_Valid()
+        {
+            // Arrange
+            var validDto = new NewsDto
+            {
+                Title = "Title",
+                Text = "Text",
+                URL = "url",
+                CreationDate = System.DateTime.UtcNow.AddMinutes(-1),
+            };
+            var command = new CreateNewsCommand(validDto);
+
+            // Act
+            var result = _validator.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatR/Newss/Delete/DeleteNewsCommandValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Streetcode.XUnitTest.MediatR.Newss.Delete
+{
+    using FluentValidation.TestHelper;
+    using Streetcode.BLL.MediatR.Newss.Delete;
+    using Xunit;
+
+    public class DeleteNewsCommandValidatorTests
+    {
+        private readonly DeleteNewsCommandValidator _validator;
+
+        public DeleteNewsCommandValidatorTests()
+        {
+            _validator = new DeleteNewsCommandValidator();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Should_Have_Error_When_Id_Is_Invalid(int id)
+        {
+            var command = new DeleteNewsCommand(id);
+            var result = _validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(x => x.id)
+                  .WithErrorMessage("Id новини має бути більше 0");
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Id_Is_Valid()
+        {
+            var command = new DeleteNewsCommand(1);
+            var result = _validator.TestValidate(command);
+            result.ShouldNotHaveValidationErrorFor(x => x.id);
+        }
+    }
+}
