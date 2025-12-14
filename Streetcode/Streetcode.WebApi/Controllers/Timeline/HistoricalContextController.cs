05f7f684794f5d1a1cd8081f108d1d253bac8cf5
Antonina Ivanova <a.ivanova.lviv@gmail.com>
2025-12-14 12:31:27 +0200

    feat: add CRUD endpoints to HistoricalContextController
    Issue #128

--- Streetcode/Streetcode.WebApi/Controllers/Timeline/HistoricalContextController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Timeline/HistoricalContextController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.DTO.Timeline;
+using Streetcode.BLL.MediatR.Timeline.HistoricalContext.Create;
+using Streetcode.BLL.MediatR.Timeline.HistoricalContext.Delete;
 using Streetcode.BLL.MediatR.Timeline.HistoricalContext.GetAll;
+using Streetcode.BLL.MediatR.Timeline.HistoricalContext.Update;
 
 namespace Streetcode.WebApi.Controllers.Timeline
 {
@@ -9,6 +13,24 @@ namespace Streetcode.WebApi.Controllers.Timeline
         public async Task<IActionResult> GetAll()
         {
             return HandleResult(await Mediator.Send(new GetAllHistoricalContextQuery()));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateHistoricalContextDto historicalContext)
+        {
+            return HandleResult(await Mediator.Send(new CreateHistoricalContextCommand(historicalContext)));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateHistoricalContextDto historicalContext)
+        {
+            return HandleResult(await Mediator.Send(new UpdateHistoricalContextCommand(historicalContext)));
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            return HandleResult(await Mediator.Send(new DeleteHistoricalContextCommand(id)));
         }
     }
 }
