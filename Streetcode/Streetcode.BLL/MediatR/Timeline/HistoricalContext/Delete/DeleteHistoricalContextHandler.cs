3bc9ad74fc8e8d82554591e4bb1c185cdb01c203
Antonina Ivanova <a.ivanova.lviv@gmail.com>
2025-12-14 11:14:35 +0200

    feat: implement DeleteHistoricalContext command and handler
    Issue #128

---
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/HistoricalContext/Delete/DeleteHistoricalContextHandler.cs
@@ -0,0 +1,45 @@
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Timeline.HistoricalContext.Delete
+{
+    public class DeleteHistoricalContextHandler : IRequestHandler<DeleteHistoricalContextCommand, Result<Unit>>
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
+
+        public DeleteHistoricalContextHandler(IRepositoryWrapper repositoryWrapper, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _logger = logger;
+        }
+
+        public async Task<Result<Unit>> Handle(DeleteHistoricalContextCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var historicalContext = await _repositoryWrapper.HistoricalContextRepository
+                    .GetFirstOrDefaultAsync(hc => hc.Id == request.Id);
+
+                if (historicalContext is null)
+                {
+                    const string errorMsg = "Historical context was not found";
+                    _logger.LogError(request, errorMsg);
+                    return Result.Fail(errorMsg);
+                }
+
+                _repositoryWrapper.HistoricalContextRepository.Delete(historicalContext);
+                await _repositoryWrapper.SaveChangesAsync();
+                
+                return Result.Ok(Unit.Value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(request, ex.Message);
+                return Result.Fail(ex.Message);
+            }
+        }
+    }
+}
