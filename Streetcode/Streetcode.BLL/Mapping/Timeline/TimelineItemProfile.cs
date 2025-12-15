fdc7a87fb42e2c141124365961dfbd5420a7d0cd
Antonina Ivanova <a.ivanova.lviv@gmail.com>
2025-12-15 11:28:53 +0200

    feat: update AutoMapper profiles for Timeline DTOs
    Issue #128

--- Streetcode/Streetcode.BLL/Mapping/Timeline/TimelineItemProfile.cs
+++ Streetcode/Streetcode.BLL/Mapping/Timeline/TimelineItemProfile.cs
@@ -8,8 +8,6 @@ public class TimelineItemProfile : Profile
 {
     public TimelineItemProfile()
     {
-        CreateMap<TimelineItem, TimelineItemDto>().ReverseMap();
-
         CreateMap<TimelineItem, TimelineItemDto>()
             .ForMember(dest => dest.HistoricalContexts, opt => opt.MapFrom(x => x.HistoricalContextTimelines
                 .Select(x => new HistoricalContextDto
@@ -17,5 +15,17 @@ public class TimelineItemProfile : Profile
                     Id = x.HistoricalContextId,
                     Title = x.HistoricalContext.Title
                 }).ToList()));
+
+        CreateMap<CreateTimelineItemDto, TimelineItem>()
+            .ForMember(dest => dest.HistoricalContextTimelines, opt => opt.Ignore());
+
+        CreateMap<UpdateTimelineItemDto, TimelineItem>()
+            .ForMember(dest => dest.HistoricalContextTimelines, opt => opt.Ignore())
+            .ForMember(dest => dest.Streetcode, opt => opt.Ignore());
+
+        CreateMap<CreateHistoricalContextDto, HistoricalContext>();
+        CreateMap<UpdateHistoricalContextDto, HistoricalContext>()
+            .ForMember(dest => dest.HistoricalContextTimelines, opt => opt.Ignore());
+        CreateMap<HistoricalContext, HistoricalContextDto>();
     }
 }
