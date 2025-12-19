f59bc2ee13ca294de49aece91963733875fd129c
Shribak Bohdan <shribakb@gmail.com>
2025-12-19 22:38:21 +0200

    feat/99: add some configurations for Streetcode.Auth.Application project

---
+++ Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Application/MediatR/Logout/LogoutHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.Auth.Application.MediatR.Logout
+{
+    internal class LogoutHandler
+    {
+    }
+}
