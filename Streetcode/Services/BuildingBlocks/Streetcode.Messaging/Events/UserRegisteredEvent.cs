a019a4636dd2afabd87abfedcf1c3bf3dbe2228b
Shribak Bohdan <shribakb@gmail.com>
2025-12-22 22:10:31 +0200

    feat/99: configure new project BuildingBlocks/Streetcode.Messaging for less duplicatoins of code and best practices to do microservices

---
+++ Streetcode/Services/BuildingBlocks/Streetcode.Messaging/Events/UserRegisteredEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Streetcode.Messaging.Events
+{
+    public record UserRegisteredEvent(
+        int UserId,
+        string Email,
+        string Name,
+        string Surname,
+        DateTime RegisteredAtUtc);
+}
