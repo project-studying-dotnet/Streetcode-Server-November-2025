6ba6b6f2a2e96b53b80b6d8facae9eb4c32ce8bc
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-12-22 14:56:44 +0200

    feat/206: implement part of Comment entity for streetcode functionality

---
+++ Streetcode/Streetcode.DAL/Entities/Streetcode/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Streetcode.DAL.Entities.Users;
+
+namespace Streetcode.DAL.Entities.Streetcode
+{
+    [Table("comments", Schema = "streetcode")]
+
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public User User { get; set; } = null!;
+        public int StreetcodeId { get; set; }
+        public StreetcodeContent Streetcode { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
