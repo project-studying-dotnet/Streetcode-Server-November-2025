d2c3fb674f61e353c1bed1a2547e609100628b06
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-12-23 01:11:42 +0200

    feat/206: implement GetCommentsByStreetcodeId functionality in CommentsController and related handler/query

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Comments/GetByStreetcodeId/GetCommentsByStreetcodeIdHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.Comments;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.Fact.GetByStreetcodeId;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Comments.GetByStreetcodeId
+{
+    public class GetCommentsByStreetcodeIdHandler : IRequestHandler<GetCommentsByStreetcodeIdQuery, Result<IEnumerable<CommentDto>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
+
+        public GetCommentsByStreetcodeIdHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Result<IEnumerable<CommentDto>>> Handle(GetCommentsByStreetcodeIdQuery request, CancellationToken cancellationToken)
+        {
+            var comments = await _repositoryWrapper.CommentsRepository
+                .GetAllAsync(c => c.StreetcodeId == request.streetcodeId);
+
+            return Result.Ok(_mapper.Map<IEnumerable<CommentDto>>(comments));
+        }
+    }
+}
---
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/CommentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.MediatR.Streetcode.Comments.GetByStreetcodeId;
+
+namespace Streetcode.WebApi.Controllers.Streetcode
+{
+    public class CommentsController : BaseApiController
+    {
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetByStreetcodeId(int streetcodeId)
+        {
+            return HandleResult(await Mediator.Send(new GetCommentsByStreetcodeIdQuery(streetcodeId)));
+        }
+    }
+}
