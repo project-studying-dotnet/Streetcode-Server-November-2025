58b32600fdc5633d628c520c559f485f303e3f70
Shribak Bohdan <shribakb@gmail.com>
2025-12-22 22:35:00 +0200

    feat/99: finish Infrastructure layer

---
+++ Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Infrastructure/Data/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streetcode.Auth.Domain.Entities.Auth;
+
+namespace Streetcode.Auth.Infrastructure.Data.Configurations
+{
+    internal class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder
+                .HasKey(r => r.Id);
+
+            builder
+                .Property(r => r.Token)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder
+                .HasIndex(r => r.Token)
+                .IsUnique();
+
+            builder
+                .Property(r => r.ExpiresAt)
+                .IsRequired();
+
+            builder
+                .Property(r => r.IsRevoked)
+                .HasDefaultValue(false);
+
+            builder
+                .Property(r => r.UserId)
+                .HasMaxLength(450)
+                .IsRequired();
+
+            builder
+                .HasOne(r => r.User)
+                .WithMany(u => u.RefreshTokens)
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
