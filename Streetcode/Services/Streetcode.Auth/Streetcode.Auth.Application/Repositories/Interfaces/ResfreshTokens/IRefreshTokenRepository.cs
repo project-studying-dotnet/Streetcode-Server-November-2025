ea2da4e25f7741492c5fb1fd6a4370b7c8014620
Shribak Bohdan <shribakb@gmail.com>
2025-12-24 12:42:39 +0200

    feat/99: finish Infrastructure layer

--- Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Application/Repositories/Interfaces/ResfreshTokens/IRefreshTokenRepository.cs
+++ Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Application/Repositories/Interfaces/ResfreshTokens/IRefreshTokenRepository.cs
@@ -5,5 +5,16 @@ namespace Streetcode.Auth.Application.Repositories.Interfaces.ResfreshTokens
 {
     public interface IRefreshTokenRepository : IRepositoryBase<RefreshToken>
     {
+        Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken);
+
+        Task<RefreshToken?> GetByTokenAsync(string token, CancellationToken cancellationToken);
+
+        Task<int> BulkRevokeExpiredTokensAsync(DateTime now, CancellationToken cancellationToken);
+
+        Task UpdateAsync(RefreshToken refreshToken, CancellationToken cancellationToken);
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+        Task<int> BulkDeleteRevokedTokensAsync(CancellationToken cancellationToken = default);
     }
 }
--- Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Infrastructure/Repositories/Realizations/RefreshTokens/RefreshTokenRepository.cs
+++ Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Infrastructure/Repositories/Realizations/RefreshTokens/RefreshTokenRepository.cs
@@ -1,4 +1,5 @@
-﻿using Streetcode.Auth.Application.Repositories.Interfaces.ResfreshTokens;
+﻿using Microsoft.EntityFrameworkCore;
+using Streetcode.Auth.Application.Repositories.Interfaces.ResfreshTokens;
 using Streetcode.Auth.Domain.Entities.Auth;
 using Streetcode.Auth.Infrastructure.Data;
 using Streetcode.BuildingBlocks.Repositories.Realizations.Base;
@@ -7,9 +8,52 @@ namespace Streetcode.Auth.Infrastructure.Repositories.Realizations.RefreshTokens
 {
     public class RefreshTokenRepository : RepositoryBase<RefreshToken>, IRefreshTokenRepository
     {
-        public RefreshTokenRepository(UsersDbContext context)
-            : base(context)
+        private readonly UsersDbContext _dbContext;
+
+        public RefreshTokenRepository(UsersDbContext dbContext)
+            : base(dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken)
+        {
+            await _dbContext.RefreshTokens.AddAsync(refreshToken, cancellationToken);
+        }
+
+        public async Task<RefreshToken?> GetByTokenAsync(string token, CancellationToken cancellationToken)
+        {
+            return await _dbContext.RefreshTokens
+                .Include(rt => rt.User)
+                .SingleOrDefaultAsync(rt => rt.Token == token, cancellationToken);
+        }
+
+        public async Task<int> BulkRevokeExpiredTokensAsync(DateTime now, CancellationToken cancellationToken)
+        {
+            return await _dbContext.RefreshTokens
+                .Where(rt => !rt.IsRevoked && rt.ExpiresOn < now)
+                .ExecuteUpdateAsync(updates => updates
+                        .SetProperty(rt => rt.IsRevoked, rt => true),
+                    cancellationToken);
+        }
+
+        public Task UpdateAsync(RefreshToken refreshToken, CancellationToken cancellationToken)
+        {
+            _dbContext.RefreshTokens.Update(refreshToken);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<int> BulkDeleteRevokedTokensAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.RefreshTokens
+                .Where(rt => rt.IsRevoked)
+                .ExecuteDeleteAsync(cancellationToken);
         }
     }
 }
