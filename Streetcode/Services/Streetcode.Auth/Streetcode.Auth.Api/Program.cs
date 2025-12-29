58e255aae8c148d0ca7350099996c7425938dec0
Shribak Bohdan <shribakb@gmail.com>
2025-12-29 17:59:04 +0200

    feat/259: use new extension method in Program.cs

--- Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Api/Program.cs
+++ Streetcode/Services/Streetcode.Auth/Streetcode.Auth.Api/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Streetcode.Auth.Api.Extensions;
+using Streetcode.Auth.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
 builder.Services.AddMessaging(builder.Configuration);
+builder.Services.AddOtlp(builder.Configuration);
 
 
 builder.Services.AddJwtAuthentication(builder.Configuration);
@@ -23,6 +26,13 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.ConfigureSerilog(builder);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UsersDbContext>();
+    db.Database.Migrate();
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.EnvironmentName == "Development")
@@ -37,7 +47,7 @@ else
 
 app.UseCors();
 
-//await app.Services.SeedIdentityAsync(); // uncomment for seeding data
+await app.Services.SeedIdentityAsync(); // uncomment for seeding data
 
 app.UseHttpsRedirection();
 
