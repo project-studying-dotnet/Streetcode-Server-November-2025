a7149c747d020c22ad3beacb8960686a56a5efa9
ArturOleksiuk <artyr27052005@gmail.com>
2025-12-30 02:35:47 +0200

    feat/264: implement GetCommentById

---
+++ Streetcode/Streetcode.DAL/Specifications/Comments/CommentWithRepliesSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using Streetcode.DAL.Entities.Streetcode;
+
+public class CommentWithRepliesSpecification : Specification<Comment>
+{
+    public CommentWithRepliesSpecification(int commentId)
+    {
+        Query
+            .Where(c => c.Id == commentId && !c.IsDeleted)
+            .Include(c => c.Replies.Where(r => !r.IsDeleted));
+    }
+}
