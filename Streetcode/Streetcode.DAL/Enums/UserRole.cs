50db332aff56fd06095c399effb75980487c2983
LekhivOleh <oleh.lekhiv.ir.2023@lpnu.ua>
2025-12-30 13:08:23 +0200

    fix/281: initialize flags in roles enums

--- Streetcode/Streetcode.DAL/Enums/UserRole.cs
+++ Streetcode/Streetcode.DAL/Enums/UserRole.cs
@@ -1,11 +1,11 @@
-﻿namespace Streetcode.DAL.Enums
+namespace Streetcode.DAL.Enums
 {
     [Flags]
     public enum UserRole
     {
-        None,
-        MainAdministrator,
-        Administrator,
-        Moderator
+        None = 0,
+        MainAdministrator = 1,
+        Administrator = 2,
+        Moderator = 3
     }
 }
